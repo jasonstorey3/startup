2530a63171083a0eb1646f363b41fbce6a236e08
jasonstorey3 <jasonbstorey3@gmail.com>
2023-03-10 23:29:30 -0700

    final push

--- home.js
+++ home.js
@@ -10,6 +10,7 @@
 window.addEventListener('DOMContentLoaded', event => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    const mapIframe = document.body.querySelector('iframe');
     if (sidebarToggle) {
         if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
             document.body.classList.toggle('sb-sidenav-toggled');
@@ -18,38 +19,13 @@ window.addEventListener('DOMContentLoaded', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+                mapIframe.setAttribute("width", "1235x")
+                console.log(mapIframe.getAttribute("width"))
+            } else {
+                mapIframe.setAttribute("width", "1000px")
+                console.log(mapIframe.getAttribute("width"))
+            }
         });
     }
-});
-
-// Get references to the sidebar toggle button and the page content wrapper
-const sidebarToggle = document.getElementById('sidebarToggle');
-const pageContentWrapper = document.getElementById('page-content-wrapper');
-
-// Add an event listener to the sidebar toggle button
-sidebarToggle.addEventListener('click', () => {
-    // Toggle the "toggled" class on the page content wrapper
-    pageContentWrapper.classList.toggle('toggled');
-
-    // Get a reference to the iframe containing the map
-    const mapIframe = document.querySelector('iframe');
-
-    // If the sidebar is now toggled, increase the width of the map iframe
-    if (pageContentWrapper.classList.contains('toggled')) {
-        mapIframe = "calc(100% - 250px)";
-    } else {
-        mapIframe = "1000px";
-    }
-});
-
-// Show the events sidebar when the Events button is clicked
-$("#events-sidebar-toggle").click(function (e) {
-    e.preventDefault();
-    $("#events-sidebar-wrapper").toggleClass("toggled");
-});
-
-// Hide the events sidebar when the close button is clicked
-$("#events-sidebar-close").click(function (e) {
-    e.preventDefault();
-    $("#events-sidebar-wrapper").toggleClass("toggled");
-});
+})
